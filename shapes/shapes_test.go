38781eae0d9a1051fa86740f22f06ed7062c028a
Ritesh Karankal <ritesh.karankal200@gmail.com>
2024-05-23 15:38:16 +0530

    table driven tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -2,34 +2,55 @@ package shapes
 
 import "testing"
 
-
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
-	want := 40.0
+	hasArea := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	if got != hasArea {
+		t.Errorf("got %.2f hasArea %.2f", got, hasArea)
 	}
 }
 
+// func TestArea(t *testing.T) {
+
+// 	checkArea := func(t testing.TB, shape Shape, hasArea float64) {
+// 		t.Helper()
+// 		got := shape.Area()
+// 		if got != hasArea {
+// 			t.Errorf("got %g hasArea %g", got, hasArea)
+// 		}
+// 	}
+// 	t.Run("rectangle", func(t *testing.T) {
+// 		rectangle := Rectangle{10.0, 10.0}
+// 			checkArea(t, rectangle, 72.0)
+
+// 	})
+
+// 	t.Run("circle", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		checkArea(t, circle, 314.1592653589793)
+// 	})
+// }
+
 func TestArea(t *testing.T) {
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 72.0)
-		
-	})
 
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
+	}
 }
